17002ecd1192147117e7225d6e7246ec19a94f1b
tarun14324 <tarunlanka7@gmail.com>
2022-07-07 11:36:45 +0530

    Retrofit api services addeds

--- app/src/main/java/com/example/facebook/util/BaseViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.facebook.util
-
-import androidx.lifecycle.ViewModel
-
-open class BaseViewModel:ViewModel() {
-}
--- app/src/main/java/com/example/facebook/viewmodels/ProfilePageViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.facebook.viewmodels
-
-import com.example.facebook.util.BaseViewModel
-
-class ProfilePageViewModel:BaseViewModel() {
-
-}
